8fd173880df91882fd01f77755e539b1e6313ce2
Bartek Antosik <antosik.bartek@gmail.com>
2013-10-12 16:11:17 +0200

    Added parsing simple json format for writing multi mode projects

--- JsonProjectReader.py
+++ JsonProjectReader.py
@@ -1,7 +1,19 @@
+from collections import defaultdict
 import json
+import MultiModeClasses
+
 
 __author__ = 'bartek'
 
+
+class ResourceParser(object):
+    RENEWABLE_RESOURCES_TAG = "renewable_resources"
+    NON_RENEWABLE_RESOURCES_TAG = "non_renewable_resources"
+    def __init__(self, jsonDictionary):
+        self.resources = {int(i): jsonDictionary[i] for i in jsonDictionary}
+
+    def parse(self):
+        return self.resources
 
 class ProjectParser(object):
     def parse(self):
@@ -11,12 +23,78 @@ class JSONParsingError(Exception):
     pass
 
 
-class MultimodeParser(ProjectParser):
-    def __init__(self, jsonDictionary):
-        pass
+class ModeParser(object):
+    DURATION_TAG = "duration"
+    DEMAND_TAG = "demand"
+    NONRENEWABLE_DEMAND = "non_renewable_demand"
+    def __init__(self, name, jsonDictionary):
+        d = jsonDictionary[self.DURATION_TAG]
+        demand = ResourceParser(jsonDictionary[self.DEMAND_TAG]).parse()
+        if self.NONRENEWABLE_DEMAND in jsonDictionary:
+            non_renewable_demand = ResourceParser(jsonDictionary[self.NONRENEWABLE_DEMAND]).parse()
+            self.mode = MultiModeClasses.Mode(name, d, demand, non_renewable_demand)
+        else:
+            self.mode = MultiModeClasses.Mode(name, d, demand)
 
     def parse(self):
-        pass
+        return self.mode
+
+
+class ActivityParser(object):
+    def __init__(self, name, jsonDictionary):
+        modes = []
+        for k,v in jsonDictionary.iteritems():
+            mode = ModeParser(k, v).parse()
+            modes.append(mode)
+        self.activity = MultiModeClasses.Activity(name, modes)
+
+    def parse(self):
+        return self.activity
+
+
+class ActivitiesParser(object):
+    ACTIVITIES_TAG = "activities"
+    def __init__(self, jsonDictionary):
+        self.result = {}
+        for key, activityJson in jsonDictionary.iteritems():
+            self.result[key] = ActivityParser(key, activityJson).parse()
+
+    def parse(self):
+        return self.result
+
+
+class RelationParser(object):
+    GRAPH_PRECEDENCE_TAG = "activity_graph"
+    def __init__(self, jsonDictionary):
+        self.jsonDictionary = jsonDictionary
+
+    def parse(self):
+        return self.jsonDictionary
+
+
+class MultimodeParser(ProjectParser):
+    def __init__(self, jsonDictionary):
+        self.jsonDictionary = jsonDictionary
+
+    def parse(self):
+        parser = ResourceParser(self.jsonDictionary[ResourceParser.RENEWABLE_RESOURCES_TAG])
+        renewable_resources = parser.parse()
+        parser = ResourceParser(self.jsonDictionary[ResourceParser.NON_RENEWABLE_RESOURCES_TAG])
+        non_renewable_resources = parser.parse()
+        activity_dictionary = ActivitiesParser(self.jsonDictionary[ActivitiesParser.ACTIVITIES_TAG]).parse()
+        relation_dictionary = RelationParser(self.jsonDictionary[RelationParser.GRAPH_PRECEDENCE_TAG]).parse()
+        activity_graph = self._createGraph(activity_dictionary, relation_dictionary)
+        return MultiModeClasses.Problem(activity_graph, renewable_resources, non_renewable_resources)
+
+    def _createGraph(self, activity_dictionary, relation_dictionary):
+        resulting_graph = defaultdict(list)
+        for preceding_activity, list_of_next in relation_dictionary:
+            try:
+                for successor in list_of_next:
+                    resulting_graph[activity_dictionary[preceding_activity]].append(activity_dictionary[successor])
+            except KeyError:
+                raise JSONParsingError("Cannot find corresponding activity in relation")
+        return resulting_graph
 
 class SinglemodeParser(ProjectParser):
     def __init__(self, jsonDictionary):
@@ -41,7 +119,7 @@ class JSONProjectReader(object):
         :rtype:
         """
         with open(filename) as file:
-            content = file.readall()
+            content = "".join(file.readlines())
             self._rawJSONContent = json.loads(content)
             parser = self.retrieveType()
             return parser.parse()
--- test_JSONProjectReader.py
+++ test_JSONProjectReader.py
@@ -1,12 +1,98 @@
+import json
+from operator import attrgetter
 from unittest import TestCase
-from JsonProjectReader import JSONProjectReader
+from JsonProjectReader import JSONProjectReader, ResourceParser, ActivitiesParser, ActivityParser, ModeParser
 import os
+import MultiModeClasses
 
 __author__ = 'bartek'
 
 FILENAME = 'testFiles/sampleProject.json'
 
+
 class TestJSONProjectReader(TestCase):
     def test_read(self):
         jsonReader = JSONProjectReader()
-        jsonReader.read(os.path.realpath(FILENAME))
+        problem = jsonReader.read(os.path.realpath(FILENAME))
+        self.assertIsInstance(problem, MultiModeClasses.Problem)
+        self.assertEqual(set([act.name for act in problem.activities_set]), set(str(i) for i in range(8)))
+        modelist = [len(act.mode_list) for act in sorted(problem.non_dummy_activities(), key=attrgetter('name'))]
+        self.assertEqual(modelist, [1, 2, 2, 2, 2, 2, 2, 1])
+
+
+class TestResourceParser(TestCase):
+    def test_parse(self):
+        jsonDictionary = json.loads("{\"1\": 4}")
+        p = ResourceParser(jsonDictionary)
+        self.assertEqual(p.parse(), {1: 4})
+
+
+class TestActivitiesParser(TestCase):
+    def test_parse(self):
+        activities = """
+        {
+            "0" : {
+                "mode1" : {
+                    "duration" : 0,
+                    "demand" : {"1": 0,"2": 0}
+                }
+            },
+            "1" : {
+                "mode1" : {
+                    "duration" : 3,
+                    "demand" : {"1": 2, "2": 5}
+                },
+                "mode2" : {
+                    "duration" : 4,
+                    "demand" : {"1": 1, "2": 1}
+                }
+            }
+        }
+        """
+        jsonDictionary = json.loads(activities)
+        p = ActivitiesParser(jsonDictionary)
+        activity_dictionary = p.parse()
+        self.assertEqual(len(activity_dictionary), 2)
+        for k, v in activity_dictionary.iteritems():
+            self.assertIsInstance(v, MultiModeClasses.Activity)
+            self.assertEqual(k, v.name)
+
+
+class TestActivityParser(TestCase):
+    def test_parse(self):
+        activity_string = """
+            {
+                "mode1" : {
+                    "duration" : 3,
+                    "demand" : {"1": 2, "2": 5}
+                },
+                "mode2" : {
+                    "duration" : 4,
+                    "demand" : {"1": 1, "2": 1}
+                }
+            }
+            """
+        jsonDictionary = json.loads(activity_string)
+        p = ActivityParser("somename", jsonDictionary)
+        a = p.parse()
+        self.assertIsInstance(a, MultiModeClasses.Activity)
+        self.assertEqual(a.maximal_duration(), 4)
+        self.assertEqual(a.minimal_duration(), 3)
+        self.assertEqual(len(a.mode_list), 2)
+        self.assertEqual(a.name, "somename")
+
+
+class TestModeParser(TestCase):
+    def test_parse(self):
+        mode_string = """
+        {
+            "duration" : 4,
+            "demand" : {"1": 1, "2": 1}
+        }
+        """
+        jsonDictionary = json.loads(mode_string)
+        p = ModeParser("somename", jsonDictionary)
+        mode = p.parse()
+        self.assertIsInstance(mode, MultiModeClasses.Mode)
+        self.assertEqual(mode.demand, {1: 1, 2: 1})
+        self.assertEqual(mode.duration, 4)
