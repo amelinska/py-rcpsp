12f229b1cb356a9a0f1b2482f5ec1d2c96beed6d
Bartek Antosik <antosik.bartek@gmail.com>
2013-10-21 19:24:51 +0200

    Added stub of precedence relation parser from PSPLib files

--- PSPLIBReader.py
+++ PSPLIBReader.py
@@ -1,3 +1,14 @@
+def read_precedence_relation(filecontent):
+    """
+    :param filecontent: string with psplib project description
+    :return: tuple defining precedence relation in the project, dict representing graph, and
+    string descripting dummy start, and string descripting dummy node
+    :rtype: dict[str, list[str]], str, str
+    """
+    pass
+
+
+
 class PSPLibReader(object):
     def read(self, filename):
         pass
