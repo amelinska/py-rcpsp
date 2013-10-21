020639cc963c4a85bfc6e38b0cfcd16fc6874ef0
amelinska <aleksandra.melinska@gmail.com>
2013-10-21 21:01:55 +0200

    reading psplib file: reading kinds of resuorces and capacity

--- PSPLIBReader.py
+++ PSPLIBReader.py
@@ -19,10 +19,37 @@ def read_precedence_relation(precedence_relation_section_lines):
     return result_dict
 
 
+def tokenize(label_line):
+    label_tokens = label_line.split(' ')
+    filtered_tokens = [token for token in label_tokens if len(token) > 0]
+    return filtered_tokens
 
+#TODO: write tests
+def read_resources(label_line, capacity_line):
+    filtered_tokens = tokenize(label_line)
+    labels = []
+    for i in range(0, len(filtered_tokens), 2):
+        labels.append(filtered_tokens[i] + filtered_tokens[i+1])
 
+    capacity_tokens = tokenize(capacity_line)
+    return {labels[i] : int(capacity_tokens[i]) for i in range(len(labels))}
 
-    pass
+def split_dictionary(input_dict, list_of_prefixes):
+    """
+    :param input_dict: dictionary to be splitted into list of dictionaries ; {"some1": 4, "some5": 2, "other5": 9}
+    :param list_of_prefixes: prefixes according to which input_dict will be splitted ; ["some","other"]
+    :return: list of splitted dictionaries with the same order as list_of_prefixes ; [{"some1": 4, "some5": 2}, {"other5": 9}]
+    :type input_dict: dict[str]
+    """
+    result_list = []
+    for prefix in list_of_prefixes:
+        dict_for_prefix = {}
+        for key in input_dict:
+            if key.startswith(prefix):
+            dict_for_prefix[key] = input_dict[key]
+        result_list.append(dict_for_prefix)
+    return result_list
+#TODO test for split_dictionary
 
 class PSPLibReader(object):
     def read(self, filename):
